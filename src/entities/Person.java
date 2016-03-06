218bca181e6afa3719b81bba422b4e11a03c846a
Pavel <pauluxxx@mail.ru>
2016-03-06 23:35:22 +0200

    Я сделалъ пожя посмотрите что не так

---
+++ src/entities/Person.java
@@ -0,0 +1,74 @@
+package entities;
+
+import java.util.Date;
+
+import java.io.Serializable;
+
+/**
+ * Created by Pavel Poimanov on 06.03.2016.
+ */
+public class Person implements Serializable {
+    private int id;
+    private String name;
+    private String surname;
+    private Date birth_date;
+    private Date death_date;
+    private String country;
+    private int imageId;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public Date getDeath_date() {
+        return death_date;
+    }
+
+    public void setDeath_date(Date death_date) {
+        this.death_date = death_date;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
