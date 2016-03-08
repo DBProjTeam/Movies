5a2cc5dfb74a878bb7d1c35d56d970c885d2920c
Coursework <adres199219@gmail.com>
2016-03-08 08:11:20 +0200

    Added files via upload

---
+++ src/entities/Movie.java
@@ -0,0 +1,83 @@
+package entities;
+
+import java.util.Date;
+
+/**
+ * Created by Станислав on 08.03.16.
+ */
+public class Movie {
+    private int movie_id;
+    private int runtime;
+    private int year;
+    private Date releaseDate;
+    private String language;
+    private String description;
+    private int visits;
+    private int image_id;
+
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(int movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getVisits() {
+        return visits;
+    }
+
+    public void setVisits(int visits) {
+        this.visits = visits;
+    }
+
+    public int getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(int image_id) {
+        this.image_id = image_id;
+    }
+
+}
