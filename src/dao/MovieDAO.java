4cdc3dd18a281d5c7f6235e94e12d231e694c55f
Coursework <adres199219@gmail.com>
2016-03-12 17:47:30 +0200

    Добавления функция поиска  по названию фильма

--- src/dao/MovieDAO.java
+++ src/dao/MovieDAO.java
@@ -12,9 +12,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDAO  {
-    private  static String GET_MOVIE_BY_ID="SELECT * FROM movie WHERE movie.movie_ID=?";
-    private static String GET_MOVIE_BY_YEAR="SELECT * FROM movie WHERE movie.year=?";
-    private static String GET_MOVIE_ALL ="SELECT * FROM movie";
+    private static String GET_MOVIE_BY_ID="SELECT * FROM movie WHERE movie.movie_ID=?;";
+    private static String GET_MOVIE_BY_YEAR="SELECT * FROM movie WHERE movie.year=?;";
+    private static String GET_MOVIE_ALL ="SELECT * FROM movie;";
+    private static String SEARCH_BY_TITLE="SELECT * FROM movie WHERE movie.title LIKE  ?'%';"; // Втаблице movie нет title!?
     Connection connection;
 
     public Movie getByPK(int pk) throws SQLException {
@@ -34,6 +35,25 @@ public class MovieDAO  {
             Connector.close(resultSet);
         }
         return movie;
+    }
+
+    public List<Movie> searchByTitle(String word) throws SQLException{
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        List<Movie> movies =new ArrayList<Movie>();
+        try {
+            connection = Connector.getConnection();
+            statement = connection.prepareStatement(SEARCH_BY_TITLE);
+            statement.setString(1, word);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {//Исправлено
+                movies.add(obtain(resultSet));
+            }
+        } finally {
+            Connector.close(statement);
+            Connector.close(resultSet);
+        }
+        return movies;
     }
 
     public List<Movie> getMoviesByYear(int year) throws SQLException {
