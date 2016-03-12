02f79ea403ed92bd390756a61f4b1826fc749144
Coursework <adres199219@gmail.com>
2016-03-12 18:19:07 +0200

    Добавления функция поиска  по имени и фамилии ..
    персоны

--- src/dao/PersonDAO.java
+++ src/dao/PersonDAO.java
@@ -7,12 +7,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Едик  Лисогуб on 06.03.2016.
  */
 public class PersonDAO {
     private static final String GET_PESON_BY_ID = "SELECT * FROM person WHERE person_ID=?";
+    private static final String SEARCH_PERSON_BY_NAME_AND_SURNAME="SELECT * FROM person WHERE(person.name LIKE ?'%') OR (person.surname LIKE ?'%')";
     Connection connection;
 
     public Person getByPK(int pk) throws SQLException {
@@ -27,6 +30,27 @@ public class PersonDAO {
             res = statement.executeQuery();
             if (res.next()) {
                 person = obtain(res);
+            }
+        } finally {
+            Connector.close(statement);
+            Connector.close(res);
+        }
+        return person;
+
+    }
+
+    public List<Person> searchPersonByNameAndSurname(String words) throws SQLException {
+        List<Person> person = new ArrayList<Person>();
+        PreparedStatement statement = null;
+        ResultSet res = null;
+        try {
+            connection = Connector.getConnection();
+            statement = connection.prepareStatement(SEARCH_PERSON_BY_NAME_AND_SURNAME);
+            statement.setString(1, words);
+            statement.setString(2, words);
+            res = statement.executeQuery();
+            while (res.next()) {
+                person.add(obtain(res));
             }
         } finally {
             Connector.close(statement);
