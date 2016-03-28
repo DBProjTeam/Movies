38494ce6d340215a61005e693d0a3f981ad55501
Coursework <adres199219@gmail.com>
2016-03-28 19:05:26 +0300

    Добавил класс сортировок.
    Может у кого то  есть другие более эффективные  идеи сортировки фильмом по рейтингу.

---
+++ src/util/Sort.java
@@ -0,0 +1,101 @@
+public  class  SortMovie {
+
+    private static List<MovieRating> movieRatingList = new ArrayList<MovieRating>();
+
+    public static void sortMovieByName(List<Movie> list) {
+        list.sort(new Comparator<Movie>() {
+            @Override
+            public int compare(Movie o1, Movie o2) {
+                String title1 = o1.getTitle();
+                String title2 = o2.getTitle();
+                return title1.toString().compareTo(title2.toString());
+            }
+        });
+    }
+
+    public static void sortMovieByYear(List<Movie> list) {
+        list.sort(new Comparator<Movie>() {
+            @Override
+            public int compare(Movie o1, Movie o2) {
+                int year1 = o1.getYear();
+                int year2 = o2.getYear();
+                if (year1 > year2) {
+                    return -1;
+                } else if (year1 < year2) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+    }
+
+
+    public static void sortMovieByRating(List<Movie> list) {
+        ListIterator<Movie> iterator = list.listIterator();
+        Movie movie = null;
+        while (iterator.hasNext()) {
+            movie = iterator.next();
+            movieRatingList.add(new MovieRating(movie));
+        }
+        movieRatingList.sort(new Comparator<MovieRating>() {
+            @Override
+            public int compare(MovieRating o1, MovieRating o2) {
+                if (o1.rating > o2.rating) {
+                    return -1;
+                } else if (o1.rating < o2.rating) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+
+        list.clear();
+        ListIterator<MovieRating> iteratorMovieRating = movieRatingList.listIterator();
+        MovieRating movieRating = null;
+        while (iteratorMovieRating.hasNext()) {
+            movieRating = iteratorMovieRating.next();
+            list.add(movieRating.getMovie());
+        }
+
+    }
+
+
+    private static class MovieRating {
+        private Movie movie = null;
+        private double rating = 0;
+
+        public MovieRating(Movie movie) {
+            this.movie = movie;
+            this.rating = calculationRatingMovie(movie.getMovie_id());
+        }
+
+        public double getRating() {
+            return rating;
+        }
+
+        public Movie getMovie() {
+            return movie;
+        }
+
+        private static double calculationRatingMovie(int movie_id) {
+            RatingDAO ratingDAO = new RatingDAO();
+            int count = 0;
+            int sum = 0;
+            try {
+
+                List<Rating> list = ratingDAO.getRatingAllByIdMovie(movie_id);
+                ListIterator<Rating> iterator = list.listIterator();
+                Rating rating = null;
+                while (iterator.hasNext()) {
+                    rating = iterator.next();
+                    sum += rating.getScore();
+                }
+            } catch (SQLException e) {
+            }
+            count++;
+            return ((sum * 1.0) / (count * 1.0));
+        }
+    }
+}
