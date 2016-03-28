ecbb3f047494ffc041887a841f5b654656e5bed2
Coursework <adres199219@gmail.com>
2016-03-28 15:10:09 +0300

    Added files via upload

--- src/dao/view/PersonRoleDAO.java
+++ src/dao/view/PersonRoleDAO.java
@@ -15,7 +15,8 @@ import java.util.List;
  * Created by Станислав on 09.03.16.
  */
 public class PersonRoleDAO {
-    private static String GET_PERSON_ROLE_BY_MOVIE_ID = "SELECT * FROM movie_person_role_view WHERE movie_ID=?";
+    private static String GET_PERSON_ROLE_BY_MOVIE_ID = "SELECT * FROM movie_person_role_view WHERE movie_ID=?";//запрос на выборку всех персон кто играл в фильме
+    private static String GET_PERSON_ROLE_BY_PERSON_ID = "SELECT * FROM movie_person_role_view WHERE person_ID=?";//запрос на выборку всех ролей персоны
 
     Connection connection;
 
@@ -23,8 +24,7 @@ public class PersonRoleDAO {
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         //todo так не хорошо я 20 мин искал ошибку надо инициализировать пременную прежде чем добавлять add!!!!
-        List<PersonRoleView> personRoleViews = null;
-        personRoleViews = new ArrayList<PersonRoleView>();
+        List<PersonRoleView> personRoleViews = new ArrayList<PersonRoleView>();
 
         try {
             connection = Connector.getConnection();
@@ -44,10 +44,10 @@ public class PersonRoleDAO {
     public List<PersonRoleView> getPersonRoleByPersonId(int person_id) throws SQLException {
         PreparedStatement statement = null;
         ResultSet resultSet = null;
-        List<PersonRoleView> personRoleViews = null;
+        List<PersonRoleView> personRoleViews = new ArrayList<PersonRoleView>();
         try {
             connection = Connector.getConnection();
-            statement = connection.prepareStatement(GET_PERSON_ROLE_BY_MOVIE_ID);
+            statement = connection.prepareStatement(GET_PERSON_ROLE_BY_PERSON_ID);
             statement.setInt(1, person_id);
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
