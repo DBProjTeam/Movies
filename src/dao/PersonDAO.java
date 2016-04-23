884e2f4d0fbdb1a47b690cd695b1c107527130cb
Coursework <adres199219@gmail.com>
2016-04-23 21:56:09 +0300

    Добавил inset update delete

--- src/dao/PersonDAO.java
+++ src/dao/PersonDAO.java
@@ -3,10 +3,7 @@ package dao;
 import entities.Person;
 import util.Connector;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +13,9 @@ import java.util.List;
 public class PersonDAO {
     private static final String GET_PERSON_BY_ID = "SELECT * FROM person WHERE person_ID=?";
     private static final String SEARCH_PERSON_BY_NAME_AND_SURNAME="SELECT * FROM person WHERE(person.name LIKE ?'%') OR (person.surname LIKE ?'%')";
+    private static final String INSERT_PERSON ="INSERT INTO `person` (`name`,`surname`,`birth_date`,`death_date`,`country`,`image_ID`) VALUES (?,?,?,?,?,?);";
+    private static final String UPDATE_PERSON="UPDATE `person` SET `name`=?, `surname`=?, `birth_date`=?, `death_date`=?, `country`=?,`image_ID`=? WHERE `person_ID`=?;";
+    private static final String DELETE_PERSON="DELETE FROM `person` WHERE `person_ID`=?;";
     Connection connection;
 
     public Person getByPK(int pk) throws SQLException {
@@ -59,6 +59,59 @@ public class PersonDAO {
 
     }
 
+    public boolean insert(Person person)throws SQLException{
+        boolean isOk = false;
+        PreparedStatement  statement = null;
+        try {
+            connection=Connector.getConnection();
+            statement=connection.prepareStatement(INSERT_PERSON);
+            statement.setString(1,person.getName());
+            statement.setString(2,person.getSurname());
+            statement.setDate(3, (Date) person.getBirth_date());
+            statement.setDate(4,(Date) person.getDeath_date());
+            statement.setString(5, person.getCountry());
+            statement.setInt(6,person.getImageId());
+            isOk = statement.execute();
+        }finally {
+            Connector.close(statement);
+        }
+        return isOk;
+    }
+
+    public boolean update(Person person)throws SQLException{
+        boolean isOk= false;
+        PreparedStatement statement = null;
+        try {
+            connection = Connector.getConnection();
+            statement= connection.prepareStatement(UPDATE_PERSON);
+            statement.setString(1,person.getName());
+            statement.setString(2,person.getSurname());
+            statement.setDate(3,(Date)person.getBirth_date());
+            statement.setDate(4,(Date)person.getDeath_date());
+            statement.setString(5,person.getCountry());
+            statement.setInt(6,person.getImageId());
+            statement.setInt(7,person.getId());
+            isOk= statement.execute();
+        }finally {
+            Connector.close(statement);
+        }
+        return isOk;
+    }
+
+    public boolean delete(Person person)throws SQLException{
+        boolean isOk= false;
+        PreparedStatement statement = null;
+        try{
+            connection= Connector.getConnection();
+            statement= connection.prepareStatement(DELETE_PERSON);
+            statement.setInt(1,person.getId());
+            isOk= statement.execute();
+        }finally {
+            Connector.close(statement);
+        }
+        return isOk;
+    }
+
     private Person obtain(ResultSet res) throws SQLException {
         Person person = new Person();
         person.setId(res.getInt("person_Id"));
@@ -70,4 +123,5 @@ public class PersonDAO {
         person.setDeath_date(res.getDate("death_date"));
         return person;
     }
+
 }
